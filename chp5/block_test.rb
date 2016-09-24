f6fcd7ae4871650e0b0421ecfa21f3119f774c82
Justin Welenofsky <jwelenofsky@three29.com>
2016-09-23 17:05:36 -0700

    testing blocks

---
+++ chp5/block_test.rb
@@ -0,0 +1,10 @@
+def my_method(&my_block)
+	puts "We're in method, about to invoke your block!"
+	my_block.call
+	puts "We're back in the method!"
+end
+
+my_method do
+	puts "We're in the block!"
+end
+
