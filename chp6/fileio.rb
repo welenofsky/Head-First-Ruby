b916b9ef71d0d3f9e807c9fd5560c97ebf1c4f93
Justin Welenofsky <welenofsky@gmail.com>
2016-09-25 22:29:06 -0700

    End chp6

--- chp6/fileio.rb
+++ chp6/fileio.rb
@@ -1,7 +1,22 @@
+def find_adjective(string)
+	words = string.split(" ")
+	index = words.find_index("is")
+	words[index + 1]
+end
+
 lines = []
 
 File.open("reviews.txt") do |review_file|
 	lines = review_file.readlines
 end
 
-puts lines.length
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
+reviews = relevant_lines.reject { |line| line.include?("--") }
+
+adjectives = reviews.map do |review|
+	adjective = find_adjective(review)
+	"'#{adjective.capitalize}'"
+end
+
+puts "The critics agree, Truncated is: "
+puts adjectives
