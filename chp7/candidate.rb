762b8815b4526a5b846763458083a5b5caa46974
Justin Welenofsky <welenofsky@gmail.com>
2016-09-26 17:14:15 -0700

    Keyword Arguments

--- chp7/candidate.rb
+++ chp7/candidate.rb
@@ -1,11 +1,12 @@
 class Candidate
 	attr_accessor :name, :age, :occupation, :hobby, :birthplace
-	def initialize(name, options)
+	# The hash defaults are called "Keyword Arguments", new in ruby 2.0
+	def initialize(name, age: nil, occupation: nil, hobby: nil, birthplace: "Sleepy Creek")
 		self.name = name
-		self.age = options[:age]
-		self.occupation = options[:occupation]
-		self.hobby = options[:hobby]
-		self.birthplace = options[:birtplace]
+		self.age = age
+		self.occupation = occupation
+		self.hobby = hobby
+		self.birthplace = birthplace
 	end
 end
 
